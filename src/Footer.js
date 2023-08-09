2d9d0a4e0c796500c50a6626394358cdc48e12bc
samflexx31 <samflex3124@gmail.com>
2023-08-09 12:31:59 +0100

    first commit

---
+++ src/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react'
+const today =new Date();
+
+export const Footer = ({ length }) => {
+  return (
+    <footer>
+        
+        <p>{length} list {length === 1 ? "item" : "items"}
+        
+        </p>
+
+    </footer>
+  )
+}
+
+export default Footer
